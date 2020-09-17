a2fb614bfb5830bd998aa784abe9118e228ba497
benhassenwael <44639258+benhassenwael@users.noreply.github.com>
2020-09-17 14:03:14 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,8 +8,11 @@
 int main(void)
 {
 	char c;
-	for (c = '0' ; c <= '9' ; c++) putchar (c);
-	for (c = 'a' ; c <= 'f' ; c++) putchar (c);
+
+	for (c = '0' ; c <= '9' ; c++)
+		putchar (c);
+	for (c = 'a' ; c <= 'f' ; c++)
+		putchar (c);
 	putchar('\n');
 	return (0);
 }
