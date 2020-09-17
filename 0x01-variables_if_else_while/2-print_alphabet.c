992d7023c2de48705c88e4c484a1bd339e4ad642
benhassenwael <greenw2012@gmail.com>
2020-09-17 13:37:56 +0100

    Initial commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,7 +5,8 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-	for (char c = 'a' ; c <= 'z' ; c++) putchar (c);
+	for (char c = 'a' ; c <= 'z' ; c++)
+		putchar (c);
 	putchar('\n');
 	return (0);
 }
