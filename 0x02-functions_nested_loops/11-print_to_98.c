0c80cd603c3fb4bff7f3773be460672c55b6476b
benhassenwael <greenw2012@gmail.com>
2020-09-22 10:06:11 +0100

    Initial commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "holberton.h"
 /**
- * _putchar - writes the character c to stdout
+ * print_to_98 - writes the character c to stdout
  * @c: The character to print
  *
  * Return: On success 1.
--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
+ * _isalpha - writes the character c to stdout
  * @c: The character to print
  *
  * Return: On success 1.
