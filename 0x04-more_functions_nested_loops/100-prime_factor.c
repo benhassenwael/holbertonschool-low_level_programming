c972d7f67d7999b9b02ea2947a5b5795fdd69dff
benhassenwael <greenw2012@gmail.com>
2020-09-24 13:28:54 +0100

    Initial commit

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,12 +1,36 @@
+#include <stdio.h>
+#include <math.h>
 #include "holberton.h"
 
 /**
- *  - 
- * @: 
+ * main - main func
  *
- * Return: 
+ * Return: always 0
  */
-
+int main(void)
 {
-	return ();
+	long n = 612852475143;
+	int i, max;
+
+	while(n % 2 == 0)
+	{
+		max = 2;
+		n = n / 2;
+	}
+
+	for (i = 3; i <= sqrt(n); i = i + 2)
+	{
+		while(n % i == 0)
+		{
+			max = i;
+			n = n / i;
+		}
+	}
+
+	if (n > 2)
+	{
+		max = n;
+	}
+	printf("%d\n", max);
+	return (0);
 }
