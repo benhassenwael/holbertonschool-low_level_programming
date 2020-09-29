ecdf3e4f7c7eef658c025740ea8c831005b7852e
benhassenwael <greenw2012@gmail.com>
2020-09-29 14:55:03 +0100

    Initial commit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,14 +1,39 @@
 #include "holberton.h"
 
 /**
- *  - 
- * @: 
+ * _atoi - convert string to int
+ * @s: string to convert to int
  *
- * Return: 
+ * Return: int
  */
 
-
+int _atoi(char *s)
 {
+	int sign = 1, number = 0, i = 0, continu = 1;
 
-	return();
+	while (continu && s[i])
+	{
+		if (s[i] == '-')
+			sign *= -1;
+
+		if (s[i] < 58 && s[i] > 47)
+		{
+			if (!number)
+				number = s[i] - '0';
+			else
+			{
+				number *= 10;
+				number += s[i] - '0';
+			}
+		}
+		else if (number)
+		{
+			continu = 0;
+		}
+		i++;
+	}
+
+	number *= sign;
+
+	return (number);
 }
