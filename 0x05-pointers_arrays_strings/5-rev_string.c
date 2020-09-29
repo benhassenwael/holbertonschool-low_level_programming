b6dd1c2b8ed8ddc57239dd46173a1eba20958025
benhassenwael <greenw2012@gmail.com>
2020-09-29 11:08:27 +0100

    Update

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "holberton.h"
 
 /**
@@ -15,10 +14,9 @@ void rev_string(char *s)
 	for (length = 0; s[length]; length++)
 	{
 	}
-	printf("%d \n", length);
+
 	for (i = 0, length -= 1; i < length/2; i++)
 	{
-		printf("i = %d, j = %d \n", i, length-i);
 		aux = s[i];
 		s[i] = s[length-i];
 		s[length-i] = aux;
