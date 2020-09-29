c27952b60b657e107283dca045c9495f3fd2da19
benhassenwael <greenw2012@gmail.com>
2020-09-29 21:39:37 +0000

    Initial commit

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,9 +10,10 @@ void puts2(char *str)
 {
 	int i = -2;
 
-	do {
+	while (str[i + 1] != '\0' && str[i + 2] != '\0' && str[i])
+	{
 		i += 2;
 		_putchar(str[i]);
-	} while (str[i + 1] != '\0' && str[i + 2] != '\0');
+	}
 	_putchar('\n');
 }
