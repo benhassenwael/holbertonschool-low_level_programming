62b17abff35282093b3a65d78293cb79744d40e1
benhassenwael <greenw2012@gmail.com>
2020-09-29 21:36:01 +0000

    Initial commit

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,14 +1,18 @@
 #include "holberton.h"
 
 /**
- *  - 
- * @: 
+ * puts2 - print every other character of a string
+ * @str: string to print from
  *
- * Return: 
  */
 
-
+void puts2(char *str)
 {
+	int i = -2;
 
-	return();
+	do {
+		i += 2;
+		_putchar(str[i]);
+	} while (str[i + 1] != '\0' && str[i + 2] != '\0');
+	_putchar('\n');
 }
