6fcd5143a9882c6de1c6771c0a62950044f79798
benhassenwael <greenw2012@gmail.com>
2020-10-01 19:33:14 +0000

    Initial commit

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,14 +1,29 @@
 #include "holberton.h"
 
 /**
- *  - 
- * @: 
+ * rot13 - encode a string using rot13
+ * @s: string to encode
  *
- * Return: 
+ * Return: pointer to the resulting string
  */
 
-
+char *rot13(char *s)
 {
+	char *rot = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char *alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	int i = 0, j;
 
-	return (0);
+	while (s[i])
+	{
+		j = 0
+		while (alpha[j] && s[i] != m[j])
+			j++;
+
+		if (alpha[j])
+			s[i] = rot[j];
+
+		i++;
+	}
+
+	return (s);
 }
