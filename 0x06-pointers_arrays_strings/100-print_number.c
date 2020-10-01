9bd8dd4f89e95e61c4cbe52339a27ec03718d4ed
benhassenwael <greenw2012@gmail.com>
2020-10-01 13:49:54 +0100

    Initial commit

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -1,14 +1,38 @@
 #include "holberton.h"
 
 /**
- *  - 
- * @: 
+ * print_number - print a given integer
+ * @n: int to print
  *
- * Return: 
  */
 
-
+void print_number(int n)
 {
+	int nDigits;
+	unsigned int aux, num = 0;
 
-	return (0);
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -n;
+	}
+	else
+		num = n;
+
+	aux = num / 10;
+	while (aux != 0)
+	{
+		aux /= 10;
+		nDigits *= 10;
+	}
+
+	while (nDigits != 1)
+	{
+		_putchar((num / nDigits) + '0');
+		num %= nDigits;
+		nDigits /= 10;
+	}
+
+	_putchar(num + '0');
+
 }
