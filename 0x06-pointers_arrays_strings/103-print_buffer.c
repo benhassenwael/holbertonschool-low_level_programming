9995200e198b615f27e9b5fd065c1d4e9902a33e
benhassenwael <greenw2012@gmail.com>
2020-10-01 22:41:37 +0000

    Initial commit

--- 0x06-pointers_arrays_strings/103-print_buffer.c
+++ 0x06-pointers_arrays_strings/103-print_buffer.c
@@ -1,13 +1,13 @@
 #include "holberton.h"
 
 /**
- *  - 
- * @: 
+ * print_buffer - print a buffer
+ * @b: buffer
+ * @size: buffer size
  *
- * Return: 
  */
 
-
+void print_buffer(char *b, int size)
 {
 
 	return (0);
