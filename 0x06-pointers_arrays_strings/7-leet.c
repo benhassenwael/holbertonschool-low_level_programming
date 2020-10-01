334779fb55374dddc8d98245c1abcaa0f28cd73c
benhassenwael <greenw2012@gmail.com>
2020-10-01 15:43:43 +0100

    Initial commit

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,14 +1,31 @@
 #include "holberton.h"
 
 /**
- *  - 
- * @: 
+ * leet - encodes a string into 1337
+ * @s: string to encode
  *
- * Return: 
+ * Return: pointer to the resulting string
  */
 
-
+char *leet(char *s)
 {
+	char elite[] = {'o', 'l', '\0', 'e', 'a', '\0', '\0', 't'};
+	int i = 0, j;
 
-	return (0);
+	while (s[i])
+	{
+		j = 0;
+
+		while (j < 8 && s[i] != elite[j] && s[i] != (elite[j] - ('a' - 'A')))
+		{
+			j++;
+		}
+
+		if (j != 8)
+		{
+			s[i] = j + '0';
+		}
+		i++;
+	}
+	return (s);
 }
