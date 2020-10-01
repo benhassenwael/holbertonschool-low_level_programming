7a8461722870c575aa5823f5d32d884d2007df77
benhassenwael <greenw2012@gmail.com>
2020-10-01 14:41:53 +0100

    Update

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,14 +10,14 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i;
+	int i, j = 0;
 
 	for (i = 0; dest[i]; i++)
 	{
 	}
 
-	while (*src)
-		dest[i++] = src++;
+	while (src[j])
+		dest[i++] = src[j++];
 
 	dest[++i] = '\0';
 
