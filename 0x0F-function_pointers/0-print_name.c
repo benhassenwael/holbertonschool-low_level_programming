e60a71816185d5a9655b59e2a1f4daabdf0ded10
benhassenwael <greenw2012@gmail.com>
2020-10-21 17:56:08 +0200

    Initial commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+/**
+ * print_name - prints a given name
+ * @name: string name
+ * @f: function to print name
+ *
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+/**
+ * int_index - search for an integer
+ * @array: array to search into
+ * @size: size of the array
+ * @cmp: pointer to function to be used to compare values
+ *
+ * Return: index of int or -1 if not found
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size < 1)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
+}
