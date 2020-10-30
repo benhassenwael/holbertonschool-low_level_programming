53579cc1d83b6d8abc8706e01a57e543796dcad1
benhassenwael <greenw2012@gmail.com>
2020-10-30 13:10:05 +0100

    task 6

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,18 @@
+SECTION .data
+	message: db "Hello, Holberton", 0
+	format:  db "%s", 0xA, 0
+
+SECTION .text
+extern printf
+global main
+
+main:
+	mov esi, message
+	mov rdi, format
+	mov eax, 0
+	call printf
+
+
+	mov ebx, 0
+	mov eax, 1
+	int 0x80
