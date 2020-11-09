15b080125f430b1a837ca445a18714c7fdad6b0b
benhassenwael <44639258+benhassenwael@users.noreply.github.com>
2020-11-09 15:27:42 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content)
 	{
-		for (len = 0; text_conten[len]; len++)
+		for (len = 0; text_content[len]; len++)
 			;
 		size = write(fd, text_content, len);
 	}
