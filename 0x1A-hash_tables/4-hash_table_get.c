4ccd72658ed3f6be6765f65e3bb7563d2688fdfc
benhassenwael <greenw2012@gmail.com>
2021-01-17 16:27:58 +0100

    fix: 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,6 +12,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *looper;
 
+	if (!ht || !(ht->array) || !key)
+		return (NULL);
 	index = key_index((unsigned char *)key, ht->size);
 	looper = ht->array[index];
 	while (looper)
