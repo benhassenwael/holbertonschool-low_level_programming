3ece39973726e06cace3bb688127ccc3124f3751
benhassenwael <greenw2012@gmail.com>
2021-02-19 23:50:56 +0100

    task: 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Module defining island_perimeter function """
+
+
+def island_perimeter(grid):
+    """ Counts the perimeter of the island
+        described in grid
+    """
+
+    if not grid:
+        return 0
+
+    perim = 0
+
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            if grid[row][col] == 1:
+                bot = grid[row+1][col]
+                perim += 4
+                if row > 0 and grid[row - 1][col]:
+                    perim -= 1
+                if row < len(grid) - 1 and grid[row + 1][col]:
+                    perim -= 1
+                if col > 0 and grid[row][col - 1]:
+                    perim -= 1
+                if row < len(grid[row]) - 1 and grid[row][col + 1]:
+                    perim -= 1
+    return perim
